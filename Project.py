742f358388dae5ae3d885b5dbc63552d665c180e
meomeo230 <125683733+meomeo230@users.noreply.github.com>
2023-05-15 23:38:08 +0700

    Project.py

--- Project.py
+++ Project.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+tab1, tab2, tab3, tab4 = st.tabs(["Danh sách", "Biểu đồ", "Phân nhóm", "Phân loại"])
+
+with tab1:
+    check = st.checkbox('Giới tính', ('Nam', 'Nữ'), horizontal=False)
+    radio = st.radio('Khối lớp', ('Tất cả', 'Lớp 10', 'Lớp 11', 'Lớp 12'), horizontal=False)
+
+    option = st.selectbox('Phòng', ('Tất cả', 'A114', 'A115'))
+    options = st.multiselect('Buổi', ('Sáng', 'Chiều'))
+
+if st.button('OK'):
+
+with tab2:
+    st.header("A dog")
+
+with tab3:
+    st.header("An owl")
+    
+with tab4:
+    st.header("An owl")
+
+  
