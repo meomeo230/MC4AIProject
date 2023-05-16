cefe8ff4ed5320f681749049901e1b9bbfcc7de7
meomeo230 <125683733+meomeo230@users.noreply.github.com>
2023-05-16 21:49:11 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -37,9 +37,9 @@ with tab1:
     with col1:
         st.write('Giới tính')
         if st.checkbox('Nam', value=True):
-            df[(df['CLASS'].str.contains('10')) & (df["GENDER"] == "F")]
-        if st.checkbox('Nữ', value=True):
             df[(df['CLASS'].str.contains('10')) & (df["GENDER"] == "M")]
+        if st.checkbox('Nữ', value=True):
+            df[(df['CLASS'].str.contains('10')) & (df["GENDER"] == "F")]
     with col2:
         radio = st.radio('Khối lớp', ('Tất cả', 'Lớp 10', 'Lớp 11', 'Lớp 12'), horizontal=False)
         
